31a2abe92af5af59e88a1971221ac1068b769902
Eldar Dautović <eldarcoox123@gmail.com>
2025-03-12 15:33:08 +0100

    Created routes and colors util

---
+++ lib/routes/routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const onBoarding = '/onboarding';
+  static const home = '/home';
+  static const bills = '/bills';
+  static const buy = '/buy';
+  static const wallet = '/wallet';
+  static const profile = '/profile';
+}
